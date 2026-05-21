c7f703a756a7bab837ed6daf76e8d136f33a9cb9
mar1fatih <marouanefatih631@gmail.com>
2026-05-21 21:09:10 +0100

    setting up hosted vercel api link

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -1,5 +1,5 @@
 export async function createOrder(orderData) {
-  const res = await fetch("https://bioverma.up.railway.app/api/order", {
+  const res = await fetch("https://bioverma.vercel.app/api/order", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
